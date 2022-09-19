1447046560f44c7dbbfea7e23bb8067acf351cd1
“Rebecca <rebeccaschiffman@gmail.com>
2022-09-19 16:02:01 -0700

    Finish showGIF & update button & fix bugs

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const showMeBtn = document.getElementById('showmebtn');
 const img = document.querySelector('img');
+const searchSubmitBtn = document.getElementById('searchsubmit');
+const searchInput = document.getElementById('searchbar');
 
-const showGIF = function(userSearchTerm) {
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=dNW6NhV3umI5BEbDAYmtZDp44FPquBSg&s=${userSearchTerm}`, {mode: 'cors'})
+// function to show a GIF in the img element using a search term
+const showGIF = function(searchTerm) {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=dNW6NhV3umI5BEbDAYmtZDp44FPquBSg&s=${searchTerm}`, {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
@@ -11,13 +14,23 @@ const showGIF = function(userSearchTerm) {
   });
 }
 
+// function to update show me more with search term
+const updateShowMeBtnText = function(searchTerm) {
+  showMeBtn.textContent= `Show me another ${searchTerm} GIF!`;
+}
+
+// when you submit search form, get the value
+// show a gif
+// update the show me button
+searchSubmitBtn.addEventListener('click',function(){
+  const userSearchTerm = searchInput.value;
+  showGIF(userSearchTerm);
+  updateShowMeBtnText(userSearchTerm);
+})
+
 showGIF('cats');
+updateShowMeBtnText('cats');
 
 showMeBtn.addEventListener('click',function(){
   showGIF('cats');
 })
-
-const updateShowMeBtnText = function() {
-  const searchTerm = 'cute cats';
-  showMeBtn.textContent= `Show me another ${searchTerm} GIF!`;
-}
